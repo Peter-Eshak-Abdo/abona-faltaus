99e0247bec8e86da6ea16bbf86691e5fcfe9067b
Peter-Eshak-Abdo <mixcolor11@outlook.com>
2025-01-06 19:43:33 +0200

    build structer

---
+++ app/about/page.tsx
@@ -0,0 +1,4 @@
+ function About() {
+  return <h1>Welcome to About Page!</h1>;
+}
+export default About;
---
+++ app/blog/al7an/page.tsx
@@ -0,0 +1,7 @@
+
+function Al7an() {
+  return <h1>Blog Post: Al7an</h1>;
+}
+
+
+export default Al7an;
