8cc80ae4b839d9b13d528aa9ac067e78cc1e7ca8
Peter Eshak Abdo <mixcolor11@gmail.com>
2025-03-24 20:56:05 +0200

    add 3zat babaShenody

---
+++ app/3zat/BabaShenodyPlayer.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+const sounds = [
+  {
+    monasba: "babaShenody",
+    name: "مش عارف اسمية ايه",
+    duration: "44:09",
+    src: "./3zat/babaShenody/111111.MP3",
+  },
+  {
+    monasba: "babaShenody",
+    name: "مش عارف اسمية ايه",
+    duration: "29:54",
+    src: "./3zat/babaShenody/videoplayback.mp4",
+  },
+  {
+    monasba: "babaShenody",
+    name: "مش عارف اسمية ايه",
+    duration: "20:50",
+    src: "./3zat/babaShenody/videoplayback(1).mp4",
+  },
+  {
+    monasba: "babaShenody",
+    name: "اله الضعفاء",
+    duration: "32:22",
+    src: "./3zat/babaShenody/اله الضعفاء.mp4",
+  },
+  {
+    monasba: "babaShenody",
+    name: "تأمل إجذبني وراءك فنجري",
+    duration: "17:42",
+    src: "./3zat/babaShenody/تأمل إجذبني وراءك فنجري - البابا شنودة الثالث.mp4",
+  },
+  {
+    monasba: "babaShenody",
+    name: "تأمل دفعت لأسقط",
+    duration: "15:38",
+    src: "./3zat/babaShenody/تأمل دفعت لأسقط - البابا شنودة الثالث.mp4",
+  },
+  {
+    monasba: "babaShenody",
+    name: "رحله الروح بعد الموت",
+    duration: "1:00:07",
+    src: "./3zat/babaShenodyرحله الروح بعد الموت † عظه هامه  للبابا شنوده الثالث † 1991 (192 kbps).mp3",
+  },
+  {
+    monasba: "babaShenody",
+    name: "انا سوداء و جميلة ج1",
+    duration: "43:25",
+    src: "./3zat/babaShenody/عظة قداسة البابا شنودة انا سوداء و جميلة ج1 بالموسيقى.mp4",
+  },
+];
+
+const BabaShenodyPlayer: React.FC = () => {
+  return (
+    <div
+      id="sec-babaShenody"
+      className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"
+    >
+      {sounds
+        .filter((sound) => sound.monasba === "babaShenody")
+        .map((sound, index) => (
+          <div className="col" key={`babaShenody-${index}`}>
+            <div
+              className="card shadow-sm pt-3 ps-3"
+              key={`babaShenody-${index}`}
+            >
+              <div className="d-flex justify-content-center align-items-center">
+                <audio
+                  controls
+                  is="x-audio"
+                  id={`babaShenody-audio-${index}`}
+                >
+                  <source src={sound.src} type="audio/mpeg" />
+                  متصفحك لا يدعم ملفات الصوت.
+                </audio>
+              </div>
+              <div className="card-body text-start">
+                <p className="card-text">
+                  <span className="fw-bold">ابونا: </span>داؤد لمعي
+                </p>
+                <p className="card-text">
+                  <span className="fw-bold">اسم الوعظة: </span>
+                  {sound.name}
+                </p>
+                <p className="card-text">
+                  <span className="fw-bold">تتحدث عن: </span>الصلاة
+                </p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="btn-group">
+                    <button
+                      type="button"
+                      className="btn btn-sm btn-outline-secondary"
+                    >
+                      تفاصيل
+                    </button>
+                  </div>
+                  <small className="text-body-secondary">
+                    {sound.duration}
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default BabaShenodyPlayer;
+
--- app/3zat/page.tsx
+++ app/3zat/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import AbDaodLam3yAltobaPlayer from "./AbDaodLam3yAltobaPlayer";
 import AbDaodLam3yAlslaPlayer from "./AbDaodLam3yAlslaPlayer";
+import BabaShenodyPlayer from "./BabaShenodyPlayer";
 
 function W3zat() {
   return (
@@ -11,9 +12,20 @@ function W3zat() {
       <main className="container pt-4 mt-5">
         <div className="d-flex flex-column justify-content-center align-items-center pt-5 mt-4">
           {/* <Image src="../../src/assets/media/images/img.jpg" alt="صورة لابونا فلتاؤس" className="img-thumbnail rounded" width={750} height={500} sizes="(max-width: 768px) 90vw" /> */}
-          <h1 className="display-1 text-center m-5 text-primary fw-bolder">صفحة ابونا فلتاؤس</h1>
-          <p className="fs-2 text-secondry text-center fw-light mt-5">قسم العظات الارثوذكسية المسيحية</p>
-          <img src="./images/sec1.jpeg" alt="قسم العظات" className="img-thumbnail rounded" width={750} height={500} sizes="(max-width: 768px) 90vw" />
+          <h1 className="display-1 text-center m-5 text-primary fw-bolder">
+            صفحة ابونا فلتاؤس
+          </h1>
+          <p className="fs-2 text-secondry text-center fw-light mt-5">
+            قسم العظات الارثوذكسية المسيحية
+          </p>
+          <img
+            src="./images/sec1.jpeg"
+            alt="قسم الالحان والترانيم والعظات"
+            className="img-thumbnail rounded"
+            width={400}
+            height={200}
+            sizes="(max-width: 768px) 90vw"
+          />
         </div>
         <div className="bd-example-snippet bd-code-snippet">
           <div className="bd-example m-0 border-0">
@@ -31,7 +43,11 @@ function W3zat() {
                     قسم عظات ابونا داؤد لمعي
                   </button>
                 </h4>
-                <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div
+                  id="collapseOne"
+                  className="accordion-collapse collapse"
+                  data-bs-parent="#accordionExample"
+                >
                   <div className="accordion-body">
                     <div className="accordion-item">
                       <h4 className="accordion-header">
@@ -46,7 +62,11 @@ function W3zat() {
                           قسم الصلاة
                         </button>
                       </h4>
-                      <div id="collapseAlsla" className="accordion-collapse collapse" data-bs-parent="#accordionExample1">
+                      <div
+                        id="collapseAlsla"
+                        className="accordion-collapse collapse"
+                        data-bs-parent="#accordionExample1"
+                      >
                         <div className="accordion-body">
                           <AbDaodLam3yAlslaPlayer />
                         </div>
@@ -66,7 +86,11 @@ function W3zat() {
                           قسم التوبة
                         </button>
                       </h4>
-                      <div id="collapseAltoba" className="accordion-collapse collapse" data-bs-parent="#accordionExample1">
+                      <div
+                        id="collapseAltoba"
+                        className="accordion-collapse collapse"
+                        data-bs-parent="#accordionExample1"
+                      >
                         <div className="accordion-body">
                           <AbDaodLam3yAltobaPlayer />
                         </div>
@@ -75,9 +99,7 @@ function W3zat() {
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          {/* <div className="accordion-item">
+              <div className="accordion-item">
                 <h4 className="accordion-header">
                   <button
                     className="accordion-button collapsed"
@@ -87,15 +109,21 @@ function W3zat() {
                     aria-expanded="false"
                     aria-controls="collapseTwo"
                   >
-                    قسم ترانيم عيد القيامة
+                    قسم عظات البابا شنودة
                   </button>
                 </h4>
-                <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div
+                  id="collapseTwo"
+                  className="accordion-collapse collapse"
+                  data-bs-parent="#accordionExample"
+                >
                   <div className="accordion-body">
-                    <AbDaodLam3yAltobaPlayer />
+                    <BabaShenodyPlayer />
                   </div>
                 </div>
-              </div> */}
+              </div>
+            </div>
+          </div>
         </div>
       </main>
       <Footer />
@@ -104,4 +132,3 @@ function W3zat() {
 }
 
 export default W3zat;
-
