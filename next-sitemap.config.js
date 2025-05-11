e483c9ffde7655bb326d98aba10e07538a852dd3
Peter Eshak Abdo <mixcolor11@gmail.com>
2025-05-11 21:36:47 +0300

    add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+  siteUrl: "https://abona-faltaus.vercel.app",
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/admin/*", "/api/*", "/private/*"],
+  robotsTxtOptions: {
+    additionalSitemaps: [],
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/*", "/admin/*", "/private/*"],
+      },
+    ],
+  },
+};
