ce82ff232df48d6740a8aef64adb7d73706d8585
Peter Eshak Abdo <mixcolor11@gmail.com>
2025-07-23 11:42:44 +0300

    edit file teater

--- __tests__/dummy.test.js
+++ __tests__/dummy.test.js
@@ -1,11 +1,3 @@
 test("always passes", () => {
   expect(true).toBe(true);
 });
-
-test("always fails", () => {
-  expect(true).toBe(false);
-});
-
-test("always throws an error", () => {
-  throw new Error("This is an error");
-});
