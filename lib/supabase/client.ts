ee8532fa27f9dde90ce135c86363e6faccf477b3
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-03-29 10:09:32 +0200

    anther of anther try to fix Vercel Bug with Supabase connection

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,14 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-  );
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// حماية عشان الـ Build ميفشلش لو القيم مش موجودة لحظياً
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Supabase credentials are missing!")
 }
+
+export const supabase = createBrowserClient(
+  supabaseUrl || "https://placeholder-url.supabase.co",
+  supabaseAnonKey || "placeholder-key",
+);
