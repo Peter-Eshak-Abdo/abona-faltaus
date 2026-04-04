4307118e2bb1bb2217ed6a7b15f667fe753d8d4f
Peter Eshak Abdo <mixcolor11@gmail.com>
2026-04-05 01:37:53 +0200

    fix anther errer with vercel

--- app/tranim/page.tsx
+++ app/tranim/page.tsx
@@ -8,7 +8,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
-const baseUrl = process.env.VERCEL_URL!;
+const baseUrl = process.env.VERCEL_URL || `https://${process.env.VERCEL_URL}`;;
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
